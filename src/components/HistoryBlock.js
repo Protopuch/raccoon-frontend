9f65c742e16feb783f634029438b7bbdc7257c1c
Alex-Antonov <antonovalexand@mail.ru>
2019-01-20 15:50:37 +0700

    MINOR: added css class for HistoryBlock

--- src/components/HistoryBlock.js
+++ src/components/HistoryBlock.js
@@ -1,7 +1,8 @@
 import React from "react";
+import './HistoryBlock.css'
 
 export default props => (
-    <div>
+    <div className="HistoryBlock">
         <p>{props.date} | <strong>{props.message}</strong> | <img src={props.img}/></p>
     </div>
 );
